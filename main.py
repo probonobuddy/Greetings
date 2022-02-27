3ad29706043bf88ba3b02467a01cbac80dba7f9d
mabra <mabrabson@gmail.com>
2022-02-27 14:20:18 -0500

    Added translator to portuguese and requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from greets import greetings
 from translate import Translator
 
-translator = Translator(to_lang='pt')
+translator  = Translator(to_lang='pt')
 
 for g in greetings:
     print(translator.translate(g).title())
